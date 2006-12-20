b1e02120ac20c06a5bfc8fb8c4640c119950cedf
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2006-12-20 15:26:47 +0000

    YAML エディターにコンボボックスを追加。

---
+++ com.piece_framework.piece_ide/src/com/piece_framework/piece_ide/yamleditor/editors/YAMLEditorWithCombo.java
@@ -0,0 +1,249 @@
+package com.piece_framework.piece_ide.yamleditor.editors;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IPropertyListener;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.EditorPart;
+
+/**
+ * TODO: コメント修正.
+ * @author Administrator
+ *
+ */
+public class YAMLEditorWithCombo extends EditorPart 
+                                   implements IResourceChangeListener {
+    
+    private static final int SCHEMA_COMBO_WIDTH = 200;
+    private static final int SCHEMA_COMBO_HEIGHT = 50;
+    
+    private YAMLEditor editor;
+    
+    /**
+     * TODO: コメント修正.
+     *
+     */
+    public YAMLEditorWithCombo() {
+        super();
+        editor = new YAMLEditor();
+        ResourcesPlugin.getWorkspace().addResourceChangeListener(this);
+        
+    }
+
+    /**
+     * TODO: コメント修正.
+     * 
+     * @param monitor プログレスモニタ
+     */
+    @Override
+    public void doSave(IProgressMonitor monitor) {
+        editor.doSave(monitor);
+    }
+
+    /**
+     * TODO: コメント修正.
+     */
+    @Override
+    public void doSaveAs() {
+        editor.doSaveAs();
+    }
+
+    /**
+     * TODO: コメント修正.
+     * 
+     * @param site エディターサイト
+     * @param input エディターインプット
+     * @throws PartInitException 初期化時例外
+     */
+    @Override
+    public void init(IEditorSite site, IEditorInput input)
+            throws PartInitException {
+        
+        setSite(site);
+        setInput(input);
+        setPartName(input.getName());
+    }
+
+    /**
+     * TODO: コメント修正.
+     */
+    @Override
+    public void dispose() {
+        ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
+        super.dispose();
+    }
+
+    /**
+     * TODO: コメント修正.
+     * 
+     * @return 修正
+     */
+    @Override
+    public boolean isDirty() {
+        return editor.isDirty();
+    }
+
+    /**
+     * TODO: コメント修正.
+     * 
+     * @return 修正
+     */
+    @Override
+    public boolean isSaveAsAllowed() {
+        return true;
+    }
+
+    /**
+     * TODO: コメント修正.
+     * 
+     * @param parent 親コントロール
+     */
+    @Override
+    public void createPartControl(Composite parent) {
+        try {
+            
+            RowLayout rowLayout = new RowLayout(SWT.VERTICAL);
+            rowLayout.marginRight = 0;
+            rowLayout.marginLeft = 0;
+            rowLayout.marginTop = 0;
+            rowLayout.marginBottom = 0;
+            rowLayout.marginHeight = 0;
+            rowLayout.marginWidth = 0;
+            
+            final Composite parentComposite = new Composite(parent, SWT.NONE);
+            parentComposite.setLayout(rowLayout);
+            
+            final Group schemaGroup = new Group(parentComposite, SWT.NONE);
+            schemaGroup.setText("スキーマ");
+            schemaGroup.setLayout(rowLayout);
+            
+            final Combo schemaCombo = new Combo(schemaGroup, SWT.READ_ONLY);
+            schemaCombo.setItems(new String [] {"Item 1", "Item 2", "Item 2"});
+            schemaCombo.setLayoutData(new RowData(SCHEMA_COMBO_WIDTH,
+                                                  SCHEMA_COMBO_HEIGHT));
+            
+            FillLayout fillLayout = new FillLayout(SWT.HORIZONTAL);
+            fillLayout.marginHeight = 0;
+            fillLayout.marginWidth = 0;
+            fillLayout.spacing = 0;
+            
+            final Composite editorComposite = 
+                            new Composite(parentComposite, SWT.NONE);
+            editorComposite.setLayout(fillLayout);
+            
+            editor.init(getEditorSite(), getEditorInput());
+            editor.addPropertyListener(new IPropertyListener() {
+                public void propertyChanged(Object source, int propertyId) {
+                    firePropertyChange(propertyId);
+                }
+            });
+            
+            editor.createPartControl(editorComposite);
+            
+            parent.addControlListener(new ControlListener() {
+
+                public void controlMoved(ControlEvent e) {
+                }
+
+                public void controlResized(ControlEvent e) {
+                    
+                    Point parentSize = parentComposite.getParent().getSize();
+                    
+                    editorComposite.setLayoutData(
+                            new RowData(parentSize.x, 
+                                        parentSize.y - SCHEMA_COMBO_HEIGHT));
+                    
+                }
+                
+            });
+            
+        } catch (PartInitException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * TODO: コメント修正.
+     * 
+     */
+    @Override
+    public void setFocus() {
+        editor.setFocus();
+
+    }
+
+    /**
+     * TODO: コメント修正.
+     * 
+     * @param event リソース変更イベント
+     */
+    public void resourceChanged(IResourceChangeEvent event) {
+        System.out.println(event.getType());
+        if (event.getType() == IResourceChangeEvent.POST_CHANGE) {
+            final IEditorInput input = editor.getEditorInput();
+            
+            if (input instanceof IFileEditorInput) {
+                
+                Display.getDefault().asyncExec(new Runnable() {
+                    public void run() {
+                        IFile file = ((IFileEditorInput) input).getFile();
+                        if (!file.exists()) {
+                            IWorkbenchPage page = 
+                                PlatformUI.getWorkbench().
+                                getActiveWorkbenchWindow().getActivePage();
+                            page.closeEditor(YAMLEditorWithCombo.this, false);
+                            
+                        } else if (!getPartName().equals(file.getName())) {
+                            setPartName(file.getName());
+                        }
+                    }
+                });
+            }
+        }
+        
+    }
+    
+    /**
+     * TODO: コメント修正.
+     * 
+     * @param adapter アダプター
+     * @return オブジェクト
+     */
+    @Override
+    public Object getAdapter(Class adapter) {
+       return editor.getAdapter(adapter);
+    }
+
+    /**
+     * TODO: コメント修正.
+     * 
+     * @param propertyId プロパティID
+     */
+    @Override
+    protected void firePropertyChange(int propertyId) {
+        super.firePropertyChange(propertyId);
+    }
+    
+    
+    
+}
