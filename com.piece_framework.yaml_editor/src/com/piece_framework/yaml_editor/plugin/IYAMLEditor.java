1566e2601f7b125728e893af2825c77a37c8ed1b
MATSUFUJI Hideharu <matsufuji@iteman.jp>
2007-02-10 16:09:18 +0000

    - コメントを追加した。

--- com.piece_framework.yaml_editor/src/com/piece_framework/yaml_editor/plugin/IYAMLEditor.java
+++ com.piece_framework.yaml_editor/src/com/piece_framework/yaml_editor/plugin/IYAMLEditor.java
@@ -1,5 +1,19 @@
 package com.piece_framework.yaml_editor.plugin;
 
+/**
+ * エディタークラスが実装するインターフェイス.
+ * プラグインからの通知を受け取る。
+ * 
+ * @author Hideharu Matsufuji
+ * @version 0.1.0
+ * @since 0.1.0
+ * 
+ */
 public interface IYAMLEditor {
+    
+    /**
+     * 設定の変更通知を受け取る.
+     *
+     */
     void changeProperty();
 }
